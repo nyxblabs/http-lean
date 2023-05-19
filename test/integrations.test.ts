50f356411023fcb28112977901f407731384ae37
nyxb <contact@nyxb.xyz>
2023-05-19 13:16:12 +0200

    ✨ feat(header.test.ts): add tests for header-related functions ✨ feat(integrations.test.ts): add tests for integrations with other frameworks The header.test.ts file contains tests for functions related to headers, such as getRequestHeaders, getHeaders, getRequestHeader, getHeader, setResponseHeaders, setHeaders, setResponseHeader, setHeader, appendResponseHeaders, appendHeaders, appendResponseHeader, and appendHeader. These tests ensure that the functions work as expected and can set, get, and append headers correctly.
    The integrations.test.ts file contains tests for the integration of the library with other frameworks, such as Express and Connect. These tests ensure that the library can be used as middleware in these frameworks and can wrap instances of these frameworks correctly.

---
+++ test/integrations.test.ts
@@ -0,0 +1,86 @@
+import express from 'express'
+import createConnectApp from 'connect'
+import { beforeEach, describe, expect, it } from 'vitest'
+import type { SuperTest, Test } from 'supertest'
+import supertest from 'supertest'
+import type { App } from '../src'
+import { createApp, fromNodeMiddleware, toNodeListener } from '../src'
+
+describe('integrations with other frameworks', () => {
+   let app: App
+   let request: SuperTest<Test>
+
+   beforeEach(() => {
+      app = createApp({ debug: false })
+      request = supertest(toNodeListener(app))
+   })
+
+   it('can wrap an express instance', async () => {
+      const expressApp = express()
+      expressApp.use('/', (_req, res) => {
+         res.json({ express: 'works' })
+      })
+      app.use('/api/express', fromNodeMiddleware(expressApp))
+      const res = await request.get('/api/express')
+
+      expect(res.body).toEqual({ express: 'works' })
+   })
+
+   it('can be used as express middleware', async () => {
+      const expressApp = express()
+      app.use(
+         '/api/hello',
+         fromNodeMiddleware((_req, res, next) => {
+            (res as any).prop = '42'
+            next()
+         }),
+      )
+      app.use(
+         '/api/hello',
+         fromNodeMiddleware((req, res) => ({
+            url: req.url,
+            prop: (res as any).prop,
+         })),
+      )
+      expressApp.use('/api', toNodeListener(app))
+
+      const res = await request.get('/api/hello')
+
+      expect(res.body).toEqual({ url: '/', prop: '42' })
+   })
+
+   it('can wrap a connect instance', async () => {
+      const connectApp = createConnectApp()
+      connectApp.use('/api/connect', (_req, res) => {
+         res.setHeader('content-type', 'application/json')
+         res.end(JSON.stringify({ connect: 'works' }))
+      })
+      app.use('/', fromNodeMiddleware(connectApp))
+      const res = await request.get('/api/connect')
+
+      expect(res.body).toEqual({ connect: 'works' })
+   })
+
+   it('can be used as connect middleware', async () => {
+      const connectApp = createConnectApp()
+      app.use(
+         '/api/hello',
+         fromNodeMiddleware((_req, res, next) => {
+            (res as any).prop = '42'
+            next()
+         }),
+      )
+      app.use(
+         '/api/hello',
+         fromNodeMiddleware((req, res) => ({
+            url: req.url,
+            prop: (res as any).prop,
+         })),
+      )
+      connectApp.use('/api', app)
+
+      const res = await request.get('/api/hello')
+
+      expect(res.body).toEqual({ url: '/', prop: '42' })
+   })
+})
